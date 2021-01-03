52004ec3f17768b168e3ad4469880df78ce9dbf3
mofumofuchan <you_oki1@yahoo.co.jp>
2021-01-03 19:20:37 +0900

    テンプレートで生成した型を比較するサンプルコードを追加

--- sample_function_template.cpp
+++ sample_function_template.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
@@ -8,9 +9,20 @@ T add(T a, T b) {
   return a+b;
 }
 
+template<size_t N>
+auto make_array() {
+    return array<int, N>{{}};
+}
+
 int main(void) {
   int a = 1;
   int b = 2;
   
   cout << a << " + " << b << " = " << add(a,b) << endl;
+
+  auto arr = make_array<4>();
+  auto arr2 = make_array<5>();
+
+  // type error
+  arr = arr2;
 }
